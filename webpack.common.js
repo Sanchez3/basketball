70c0bd6892c572e593999a8565b58d2180305113
硫酸 <lsch001@163.com>
2019-02-22 15:37:44 +0800

    :wrench: Fixing webpack

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,5 @@
 var webpack = require('webpack');
 var path = require('path');
-var CleanWebpackPlugin = require("clean-webpack-plugin");
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var extractCSS = new ExtractTextPlugin('assets/css/[name]-one.min.css');
@@ -9,8 +8,7 @@ var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: {
-        vendor: ['howler'],
-        main: path.resolve(__dirname, "src/assets/js/main.js")
+        app: path.resolve(__dirname, 'src/assets/js/main.js')
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -18,75 +16,76 @@ module.exports = {
     },
     module: {
         rules: [{
-            test: /(\.jsx|\.js)$/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ["es2015"]
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                },
+                exclude: /node_modules/,
+                include: '/src/'
+            }, {
+                test: /\.css$/,
+                include: /src/,
+                use: extractCSS.extract('css-loader')
+
+            }, {
+                test: /(\.scss|\.sass)$/,
+                use: extractSASS.extract(['css-loader', 'sass-loader'])
+            }, {
+                test: /\.html$/,
+                use: {
+                    loader: 'html-loader',
+                    options: {
+                        minimize: true,
+                        removeComments: false,
+                        collapseWhitespace: false
+                    }
+                }
+            }, {
+                test: /\.(gif|jpg|png|ico)\??.*$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 1024,
+                        name: '[name].[ext]',
+                        publicPath: '../../',
+                        outputPath: 'assets/css/'
+                    }
                 }
             },
-            exclude: /node_modules/,
-            include: '/src/'
-        }, {
-            test: /\.css$/,
-            include: /src/,
-            use: extractCSS.extract('css-loader')
-
-        }, {
-            test: /(\.scss|\.sass)$/,
-            use: extractSASS.extract(['css-loader', 'sass-loader'])
-        }, {
-            test: /\.html$/,
-            use: {
-                loader: 'html-loader',
-                options: {
-                    minimize: true,
-                    removeComments: false,
-                    collapseWhitespace: false
-                }
-            }
-        }, {
-            test: /\.(gif|jpg|png|ico)\??.*$/,
-            use: {
+            {
+                test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
                 loader: 'url-loader',
                 options: {
-                    limit: 1024,
+                    limit: 10000,
                     name: '[name].[ext]',
                     publicPath: '../../',
-                    outputPath: 'assets/css/'
+                    outputPath: 'assets/media/'
+                }
+            }, {
+                test: /\.(svg|woff|otf|ttf|eot)\??.*$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 1024,
+                        name: '[name].[ext]',
+                        publicPath: '../../',
+                        outputPath: 'assets/fonts/'
+                    }
                 }
             }
-        }, {
-            test: /\.(svg|woff|otf|ttf|eot)\??.*$/,
-            use: {
-                loader: 'url-loader',
-                options: {
-                    limit: 1024,
-                    name: '[name].[ext]',
-                    publicPath: '../../',
-                    outputPath: 'assets/css/'
-                }
-            }
-        }]
+        ]
     },
     plugins: [
-        //清空dist
-        new CleanWebpackPlugin(["dist"], {
-            root: '',
-            verbose: true,
-            dry: false
-        }),
         new CopyWebpackPlugin([{
-            from: path.resolve(__dirname, "src/assets/img"),
-            to: path.resolve(__dirname, "dist/assets/img")
-
+            from: path.resolve(__dirname, 'src/assets/img'),
+            to: path.resolve(__dirname, 'dist/assets/img'),
+            ignore: ['.*']
         }]),
         extractCSS,
         extractSASS,
-        new webpack.optimize.CommonsChunkPlugin({
-            names: "vendor",
-            minChunks: Infinity,
-        }),
         new HtmlWebpackPlugin({
             template: './src/index.html',
             inject: 'body',
--- webpack.dev.js
+++ webpack.dev.js
@@ -2,11 +2,20 @@ var webpack = require('webpack');
 var path = require('path');
 var merge = require('webpack-merge');
 var common = require('./webpack.common.js');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = merge(common, {
     output: {
         publicPath: '/'
     },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin(), // HMR shows correct file names in console on update.
+        new webpack.NoEmitOnErrorsPlugin(),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('development')
+        })
+    ],
     devtool: 'inline-source-map',
     devServer: {
         contentBase: path.resolve(__dirname, 'dist'),
